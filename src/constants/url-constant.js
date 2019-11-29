0bf4c9a150297df51912d7dd10396c085a918e5b
Lucas França <lucasfranca.dev@gmail.com>
2019-11-29 13:40:39 -0300

    Atualizando url

--- src/constants/url-constant.js
+++ src/constants/url-constant.js
@@ -1,4 +1,5 @@
-export const URL_API = 'http://localhost:8000'
+// export const URL_API = 'http://localhost:8000'
+export const URL_API = 'http://ec2-3-208-8-194.compute-1.amazonaws.com:8000'
 export const DATA_API = {
   'grant_type': 'password',
   'client_id': 2,
